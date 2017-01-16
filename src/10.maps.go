133f2d171ee06a325b618bd7e0b054e55d46a9ba
Mike <zhaoming23@gmail.com>
2017-01-16 14:15:16 +0800

    add maps.go

--- src/10.maps.go
+++ src/10.maps.go
@@ -8,7 +8,23 @@ func main() {
 
 	m["key1"] = 100
 	m["key2"] = 20000
-
+	m["mike"] = 999
 
 	fmt.Println("maps:", m)
+
+	v1 := m["mike"]
+	fmt.Println("v1:", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "mike")
+	fmt.Println("len:", len(m))
+	fmt.Println("maps:", m)
+
+	prs := m["mike"]
+
+	fmt.Println("prs:", prs)
+
+
+
 }
