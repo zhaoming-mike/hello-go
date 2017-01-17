093ec3226ef4bcf8dd578a912c93c480b5f82915
Mike <zhaoming23@gmail.com>
2017-01-17 17:45:52 +0800

    add channel others

---
+++ src/23.5.closing-channels.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+	go func() {
+		for{
+			j, more := <- jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j < 4; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<- done
+}
