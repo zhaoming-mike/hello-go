9dad907241069507ddf2264cdbc5e6f531a731a6
Mike <zhaoming23@gmail.com>
2017-01-17 14:49:15 +0800

    add 指针

--- src/17.pointers.go
+++ src/17.pointers.go
@@ -1,8 +1,28 @@
 package main
 import "fmt"
 
+func zeroval(ival int) {
+	ival = 0
+}
 
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
 
 func main() {
-	fmt.Println("hello pointers")
+	i := 1
+	fmt.Println("initial:", i)
+
+	//值传递
+	zeroval(i) 
+	fmt.Println("zeroval:", i)
+
+	//引用传递
+	zeroptr(&i)	
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+
+
+
 }
