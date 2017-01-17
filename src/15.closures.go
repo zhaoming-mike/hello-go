c6162376829b412912e67a0e4d52a815fd78c445
Mike <zhaoming23@gmail.com>
2017-01-17 11:09:20 +0800

    add closures

---
+++ src/15.closures.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	other := intSeq()
+
+	for i := 0; i < 10; i++ {
+		fmt.Print(other())
+		fmt.Print(", ")
+	}
+	fmt.Println("END")
+
+}
